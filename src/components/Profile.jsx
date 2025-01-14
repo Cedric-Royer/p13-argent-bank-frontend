970748cc319bbbf46f8d52d1ce525de034d5fcbc
Cedric Royer <cedricroyerpro@gmail.com>
2025-01-14 17:13:40 +0100

    modularize account sections with components

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useFetchUserProfileQuery } from '../redux/authApi';
+import AccountList from './AccountList';
 
 const Profile = () => {
   const { data, error, isLoading } = useFetchUserProfileQuery();
@@ -69,36 +70,7 @@ const Profile = () => {
       ) : (
         <p>No user data found.</p>
       )}
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-          <p className="account-amount">$2,082.79</p>
-          <p className="account-amount-description">Available Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-          <p className="account-amount">$10,928.42</p>
-          <p className="account-amount-description">Available Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section className="account">
-        <div className="account-content-wrapper">
-          <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-          <p className="account-amount">$184.30</p>
-          <p className="account-amount-description">Current Balance</p>
-        </div>
-        <div className="account-content-wrapper cta">
-          <button className="transaction-button">View transactions</button>
-        </div>
-      </section>
+      <AccountList />
     </main>
   );
 };
